71bf820e260211ad48286600f6aaf39564c31e95
Ramez Ehab <ramezehab2@gmail.com>
2024-05-08 02:07:20 +0300

    Added _layout.jsx for authentication screens

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,11 +1,18 @@
 import { Text, View } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from 'expo-router';
+import CustomButton from '../components/CustomButton'
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
       <Text className = 'text-3xl font-psemibold'>App is up</Text>
+      <CustomButton
+            title = "Continue with Email"
+            handlePress = { () => router.push('/sign_in')}
+            containerStyles = 'w-full mt-7'
+      />
       <StatusBar style="dark" />
     </SafeAreaView>
   );
