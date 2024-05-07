313679431504e8ed706de91cefef54d09c68bdc3
Ramez Ehab <ramezehab2@gmail.com>
2024-05-08 01:28:23 +0300

    Adding db.js

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env' })
+
+const mongooseConnectionPromise = mongoose.connect(process.env.MONGO_URI)
+.then(()=> {
+    console.log("MongoDB is succesfully connected")
+})
+.catch((err) => { console.log(err) })
+
+export default mongooseConnectionPromise;
