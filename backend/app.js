2b75cced32669e827802f1220c5e63869e8acb37
Ramez Ehab <ramezehab2@gmail.com>
2024-05-08 01:28:23 +0300

    Adding db.js

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,26 @@
 import express from "express";
 import dotenv from 'dotenv';
-import mongoose from 'mongoose'
+import mongooseConnectionPromise from './db.js';
 
 const app = express();
 
 dotenv.config({ path: './.env' })
 
 app.set('env', process.env.ENV);
-
 app.set('port', process.env.PORT);
 app.set('host', process.env.HOST);
 
-
-mongoose.connect(process.env.MONGO_URI)
-.then(()=> {
-    console.log("MongoDB is succesfully connected")
-})
-.catch((err) => { console.log(err) })
-
-
-app.listen(process.env.PORT, () => {
-    console.log('Server is up and running')
-    console.log(`Listening on ${process.env.HOST}:${process.env.PORT}`);
+mongooseConnectionPromise.then(() => {
+    // Database connection is established, start your Express server
+    app.listen(process.env.PORT, () => {
+        console.log('Server is up and running');
+        console.log(`Listening on ${process.env.HOST}:${process.env.PORT}`);
+    });
+}).catch((err) => {
+    // Handle error if database connection fails
+    console.error('Error connecting to MongoDB:', err);
 });
 
 app.get('/', (req, res, next) => {
     res.send('Server is up and running');
 });
-
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,24 @@ import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
     
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
 })
 
 const User = mongoose.model('User', userSchema);
