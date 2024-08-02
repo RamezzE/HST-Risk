d44d3c5825a448f1b3b04b190821656d8f5c2aa1
Ramez Ehab <ramezehab2@gmail.com>
2024-08-02 14:17:03 +0300

    style: adjust background and text color

--- frontend/app/(tabs)/news.jsx
+++ frontend/app/(tabs)/news.jsx
@@ -1,12 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 const News = () => {
   return (
-    <View>
-      <Text>News</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <Text className = "text-white">News</Text>
+    </SafeAreaView>
+  );
+};
 
-export default News
+export default News;
