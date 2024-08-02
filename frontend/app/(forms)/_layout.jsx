16c69eb43ed21cbc3d4a517a031218a72de79bcf
Ramez Ehab <ramezehab2@gmail.com>
2024-08-02 14:15:43 +0300

    style: adjust status bar color

--- frontend/app/(forms)/_layout.jsx
+++ frontend/app/(forms)/_layout.jsx
@@ -1,30 +1,33 @@
-import { View, Text } from 'react-native'
-import { Stack } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
+import { View, Text } from "react-native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 const FormsLayout = () => {
   return (
     <>
       <Stack>
-        <Stack.Screen 
-          name = 'add_team'
-          options = {{
-            headerShown : false
+        <Stack.Screen
+          name="add_team"
+          options={{
+            headerShown: false,
           }}
         />
-        <Stack.Screen 
-          name = 'edit_team'
-          options = {{
-            headerShown : false
+        <Stack.Screen
+          name="edit_team"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="add_territory"
+          options={{
+            headerShown: false,
           }}
         />
       </Stack>
-       <StatusBar 
-        backgroundColor='#161622'
-        style = 'light'
-       />
+      <StatusBar backgroundColor="#161622" style="light" />
     </>
-  )
-}
+  );
+};
 
-export default FormsLayout
+export default FormsLayout;
