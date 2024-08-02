2cbd869a04fd72008cd3e056a30bd9713927bc97
Ramez Ehab <ramezehab2@gmail.com>
2024-08-02 14:17:03 +0300

    style: adjust background and text color

--- frontend/app/(tabs)/home.jsx
+++ frontend/app/(tabs)/home.jsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <Text className = "text-white">Home</Text>
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
--- frontend/app/(tabs)/statistics.jsx
+++ frontend/app/(tabs)/statistics.jsx
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Stats = () => {
   return (
-    <View>
-      <Text>Stats</Text>
-    </View>
+    <SafeAreaView className = "bg-primary h-full">
+      <Text className = "text-white">Stats</Text>
+    </SafeAreaView>
   )
 }
 
