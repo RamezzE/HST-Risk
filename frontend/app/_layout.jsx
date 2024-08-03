abd49bf2176624f95261bedfea76067cd943f0b1
Ramez Ehab <ramezehab2@gmail.com>
2024-08-03 15:06:41 +0300

    feat: integrated google maps and polygon drawing on maps

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -30,8 +30,10 @@ const RootLayout = () => {
     return (
         <Stack>
             <Stack.Screen name="index" options = {{ headerShown: false }} />
-            {/* <Stack.Screen name="(auth)" options = {{ headerShown: false }} /> */}
-            {/* <Stack.Screen name="(tabs)" options = {{ headerShown: false }} /> */}
+            <Stack.Screen name="(auth)" options = {{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options = {{ headerShown: false }} />
+            <Stack.Screen name="(dashboard)" options = {{ headerShown: false }} />
+            <Stack.Screen name="(forms)" options = {{ headerShown: false }} />
         </Stack>
     )
 }
