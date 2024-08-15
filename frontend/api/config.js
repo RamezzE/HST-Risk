d15f2758d474a8d8713cca007d50d2381b5373d5
Ramez Ehab <ramezehab2@gmail.com>
2024-08-15 23:21:15 +0300

    api link

--- frontend/api/config.js
+++ frontend/api/config.js
@@ -1,8 +1,8 @@
 const config = {
   // serverIP: 'http://192.168.1.101:8888',
   // serverIP: 'http://192.168.1.22:8000',
-  serverIP: 'http://192.168.1.130:8888',
-  // serverIP: 'https://carmel-california.store:8443',
+  // serverIP: 'http://192.168.1.130:8888',
+  serverIP: 'https://carmel-california.store:8443',
   // serverIP: 'http://10.10.124.24:8888',
 };
 
