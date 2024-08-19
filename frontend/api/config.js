209823a77fdb90a3c5c7a4df279d59e324400f3f
Ramez Ehab <ramezehab2@gmail.com>
2024-08-19 13:51:42 +0300

    chores

--- frontend/api/config.js
+++ frontend/api/config.js
@@ -1,10 +1,5 @@
 const config = {
-  // serverIP: 'http://192.168.1.101:8888',
-  // serverIP: 'http://192.168.1.22:8000',
-  // serverIP: 'http://192.168.1.130:8888',
-  // serverIP: 'https://carmel-california.store:8443',
-  serverIP: 'http://192.168.225.218:8888'
-  // serverIP: 'http://192.168.43.218:8888'
+  serverIP: 'https://carmel-california.store:8443',
 };
 
 export default config;
