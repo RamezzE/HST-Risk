c0360c05fa5f9b1565f5d236039e9987f369db66
Ramez Ehab <ramezehab2@gmail.com>
2024-08-26 01:23:59 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ import user_router from "./routes/user.js";
 import settings_router from "./routes/settings.js";
 
 import Settings from "./models/setting.js";
+import Country from "./models/country.js";
 
 const app = express();
 
@@ -64,8 +65,59 @@ mongooseConnectionPromise
     console.error("Error connecting to MongoDB:", err);
   });
 
+
+const countries = [
+    { name: 'North Africa', teamNo: 1 },
+    { name: 'Egypt', teamNo: 3 },
+    { name: 'Central Africa', teamNo: 5 },
+    { name: 'Congo', teamNo: 1 },
+    { name: 'South Africa', teamNo: 2 },
+    { name: 'Madagascar', teamNo: 3 },
+
+    { name: 'New Guinea', teamNo: 2 },
+    { name: 'Eastern Australia', teamNo: 4 },
+    { name: 'Western Australia', teamNo: 3 },
+    { name: 'New Zealand', teamNo: 4 },
+    { name: 'Indonesia', teamNo: 4 },
+
+    { name: 'Venezuela', teamNo: 1 },
+    { name: 'Peru', teamNo: 2 },
+    { name: 'Brazil', teamNo: 4 },
+    { name: 'Bolivia', teamNo: 5 },
+    { name: 'Chile', teamNo: 1 },
+    { name: 'Paraguay', teamNo: 4 },
+    { name: 'Argentina', teamNo: 5 },
+
+    { name: 'Alaska', teamNo: 3 },
+    { name: 'Northwest Territories', teamNo: 4 },
+    { name: 'Canada', teamNo: 2 },
+    { name: 'Western US', teamNo: 5 },
+    { name: 'Eastern US', teamNo: 1 },
+    { name: 'Quebec', teamNo: 3 },
+    { name: 'Honduras', teamNo: 1 },
+    { name: 'Cuba', teamNo: 2 },
+    { name: 'Mexico', teamNo: 2 },
+    { name: 'Greenland', teamNo: 2 },
+
+    { name: 'Iceland', teamNo: 5 },
+    { name: 'Britain', teamNo: 5 },
+    { name: 'Northern Europe', teamNo: 1 },
+    { name: 'Europe', teamNo: 5 },
+    { name: 'Russia', teamNo: 1 },
+    { name: 'Kazakhstan', teamNo: 2 },
+    { name: 'Middle East', teamNo: 3 },
+    { name: 'India', teamNo: 4 },
+    { name: 'China', teamNo: 3 },
+    { name: 'Mongolia', teamNo: 2 },
+    { name: 'Korea', teamNo: 2 },
+    { name: 'Japan', teamNo: 2 },
+];
+
+
+
 app.get("/", (req, res, next) => {
   res.send("Server is up and running. HST");
+  await Country.insertMany(countries);
 });
 
 app.use("/users", user_router);
